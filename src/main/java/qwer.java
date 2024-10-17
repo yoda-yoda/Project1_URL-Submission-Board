bc47dd627df382af608523a72dd1d61374a6042b
[aslopeys941@gamail.com] <[aslopeys941@gmail.com]>
2024-10-17 22:29:19 +0900

    작성과 조회끝

---
+++ src/main/java/qwer.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+//마지막 자료부터 뭔가 이뤄진다는걸 보고 스택구조 배운것이 떠올랐음.
+
+public class qwer {
+
+    public static void main(String[] args) {
+
+        /*Map<String, String> storage = new HashMap<>();
+        storage.put("제목1", "내용내용내용1");
+        storage.put("제목2", "내용내용내용2");
+        System.out.println(storage.get("제목1"));
+        System.out.println(storage.get("제목2"));
+
+       Stack<String> storage2 = new Stack<>(); // 여기 저장되면 제일 마지막에 들어간 값을 꺼낼수있음.
+        storage2.push("aaa");
+        storage2.push("bbb");
+        System.out.println(storage2.peek());
+        storage2.pop();
+        System.out.println(storage2.peek());*/
+
+        /*Deque<String> qq1 = new LinkedList<>();
+        qq1.add("1");
+        qq1.add("2");
+        qq1.add("3");
+
+        System.out.println(qq1.peekLast());;*/
+
+        /*Map<String, String> boardStorage = new HashMap<>();
+        boardStorage.put("A", "1");
+        boardStorage.put("B", "2");
+        boardStorage.put("C", "3");
+        boardStorage.put("D", "4");
+        boardStorage.put("E", "5");
+        System.out.println(boardStorage.get("C"));; //3
+        System.out.println(boardStorage.values());*/
+
+
+        if(true){
+
+            if(false){
+
+            }
+            else if(true){
+
+            }
+
+            else{
+                System.out.println("ㅁㄴㅇ");
+            }
+        }
+
+
+
+
+    }
+
+}
--- src/main/java/qwer2.java
+++ src/main/java/qwer2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class UserInput {
+public class qwer2 { // testing class before UserInput
 
     public static void main(String[] args) {
 
@@ -8,8 +8,8 @@ public class UserInput {
         String userInput;
 
         Map<String, String> boardStorage = new HashMap<>();
-
         Deque<String> dequeStorage = new LinkedList<>();
+        Stack<String> stackStorage = new Stack<>();
 
         do { System.out.print("명령어 > ");
             userInput = sc.nextLine();
@@ -17,19 +17,23 @@ public class UserInput {
             if ( !userInput.equals("종료")){
 
                 if(userInput.equals("작성")){
-                    System.out.print("제목을 작성해주세요:");
+                    System.out.print("제목을 작성해주세요 :");
                     userInput = sc.nextLine();
-                    dequeStorage.add(userInput); // 디큐에 제목 저장
+                    dequeStorage.add(userInput); // 디큐에 제목(key) 저장
+                    stackStorage.push(userInput); // 스택에 제목(key) 저장
                     System.out.println("작성한 제목이 저장되었습니다.");
-                    System.out.print("내용을 작성해주세요:");
+                    System.out.print("내용을 작성해주세요 :");
                     userInput = sc.nextLine();
                     dequeStorage.add(userInput); //디큐에 내용 저장
                     boardStorage.put(dequeStorage.getFirst(), dequeStorage.getLast()); //Map에 제목(키), 내용(밸류) 로 저장.
                     dequeStorage.removeAll(dequeStorage); // 디큐 내용삭제. 작성 명령어가 끝난뒤 다시 작성으로 진입하면 나중에 dequeStorage 에서 값을 꺼내서 Map에 저장할때 다른것이 저장되기때문.
                     System.out.println("작성한 내용이 저장되었습니다.");
-                    System.out.println(boardStorage.get("제목1"));
 
+                }   else if(userInput.equals("조회")) { // 스택에 key를 넣은 상태임.
+                    System.out.println("제목 :[" + stackStorage.peek() + "]");
+                    System.out.println("내용 :[" + boardStorage.get(stackStorage.peek()) + "]");
                 }
+
 
                 else{
                     System.out.println("존재하지 않는 명령어 입니다.");
