296141d8edf6f0c9eacd7bca0867e1a2b49c74ca
[aslopeys941@gamail.com] <[aslopeys941@gmail.com]>
2024-10-26 15:19:24 +0900

    2-3 전부끝

--- src/main/java/qwer2.java
+++ src/main/java/qwer2.java
@@ -79,23 +79,41 @@ public class qwer2 {
                         // 원래는 맨마지막에 /를 더 추가해도 진입이 가능해서 문제였지만 맨 윗줄에서 userInputPath = userInput.split("/",3); 처럼
                         // limit을 3 설정하면서 뒤의 덩어리자체를 저장시켰기에 필터링이 가능해졌다.
 
+                        boolean existingName = false;
                         System.out.print("생성할 게시판의 이름을 입력해주세요 :");
                         userInput = sc.nextLine();
 
-                        mapKeyStorage.add(new LinkedList<>()); // 이것으로 방금 만든 게시판이 몇번째 게시판인지 그 인덱스를 활용해 알수있음.
-                        mapStorage.put(userInput, new HashMap<>()); // 게시판 저장소용 Map 에다가, 방금만든 게시판 이름을 key로 잡고 그 key에 붙는 새로운 게시판을 생성함.
-                        //이제 해당 key(게시판)으로 접근하면, 해당 게시판만의 게시물들을 String타입으로 제목(key)과 내용(vlaue)을 저장할수있음.
-                        boardKeyStorage.add(userInput); // 추가한 Map의 키를 저장.
-                        originalLocalDate.add(new LinkedList<>()); // 작성일 저장소에 인스턴스를 추가하고 인스턴스 인덱스를통해 몇번째게시판인지 알수있음. 또 인스턴스안에 링크드리스트로는 작성일과
-                        //게시글 번호랑 연결되어서 그 게시글과 작성일을 연결할수있다.
-                        editLocalDate.add(new LinkedList<>()); // 수정일 저장소도 마찬가지.
+                        for(int i=0; i < boardKeyStorage.size(); i++){ // 이름이 기존게시판과 같은지 체크하기위한 for문.
+
+                            if( userInput.equals(boardKeyStorage.get(i)) ) {   // 예외처리는 안해도된다. 사이즈0이면 for자체를실행안하니까.
+                                existingName = true; //생성에 진입못하게만듬.
+
+                                System.out.println();
+                                System.out.println("이미 해당 게시판이 존재합니다.");
+
+                                elseCheck1 = true; // 맨밑 유효하지않은 URL 이라는 출력을 막기위함.
+                                break;
+                            }
+
+                        }
+                        
+                        if(!existingName){
+
+                            mapKeyStorage.add(new LinkedList<>()); // 이것으로 방금 만든 게시판이 몇번째 게시판인지 그 인덱스를 활용해 알수있음.
+                            mapStorage.put(userInput, new HashMap<>()); // 게시판 저장소용 Map 에다가, 방금만든 게시판 이름을 key로 잡고 그 key에 붙는 새로운 게시판을 생성함.
+                            //이제 해당 key(게시판)으로 접근하면, 해당 게시판만의 게시물들을 String타입으로 제목(key)과 내용(vlaue)을 저장할수있음.
+                            boardKeyStorage.add(userInput); // 추가한 Map의 키를 저장.
+                            originalLocalDate.add(new LinkedList<>()); // 작성일 저장소에 인스턴스를 추가하고 인스턴스 인덱스를통해 몇번째게시판인지 알수있음. 또 인스턴스안에 링크드리스트로는 작성일과
+                            //게시글 번호랑 연결되어서 그 게시글과 작성일을 연결할수있다.
+                            editLocalDate.add(new LinkedList<>()); // 수정일 저장소도 마찬가지.
 
 
-                        System.out.println();
-                        System.out.printf("[%s]이 [%d]번 게시판에 저장되었습니다.", userInput, boardKeyStorage.size());
-                        System.out.println();
-                        // 방금 게시판을 저장했다면 boardKeyStorage의 사이즈가 곧 그 게시판의 번호다.
-                        elseCheck1 = true;
+                            System.out.println();
+                            System.out.printf("[%s]이 [%d]번 게시판에 저장되었습니다.", userInput, boardKeyStorage.size());
+                            System.out.println();
+                            // 방금 게시판을 저장했다면 boardKeyStorage의 사이즈가 곧 그 게시판의 번호다.
+                            elseCheck1 = true;
+                        }
 
 
                     } else if (userInputPath[1].equals("boards") &&
@@ -237,11 +255,12 @@ public class qwer2 {
                                         Map<String, String> beforeMapValue = mapStorage.get(beforeTitle);  // 수정전 게시판의 value. 즉 수정전 게시판의 게시글 제목과 내용 모음들.
                                         // 그래서 이때 beforeMapValue 는 수정전의 그 게시판 인스턴스의 메모리 주소를 담고있다.
 
-                                        boolean equalName = false;
+                                        boolean existingName = false;
+                                        boolean equalName = true;
 
                                         do{ //만약 수정한 게시판이름이 기존 게시판이름과 같다면 map저장 구조때문에 기존게시판 밸류가 삭제되므로 제한을 해주기위해 만든 do while 이다.
 
-                                            equalName = false;
+                                            existingName = false;
 
                                             System.out.printf("기존 게시판 이름 :[%s]", beforeTitle);
                                             System.out.println();
@@ -250,17 +269,29 @@ public class qwer2 {
                                             afterTitle = sc.nextLine();
                                             System.out.println();
 
-                                            for(int i=0; i < boardKeyStorage.size(); i++){
+                                            for(int i=0; i < boardKeyStorage.size(); i++){ // 게시판제목 저장소 사이즈만큼 실행하겠다.
 
                                                 if( afterTitle.equals(boardKeyStorage.get(i)) ){
-                                                    System.out.println("기존과 다른 이름을 입력해주세요");
+                                                    // 만약 변경할 제목이 기존제목과 하나라도 일치하는게 있다면 막겠다.
+                                                    if( afterTitle.equals(beforeTitle) ){
+                                                        System.out.println("기존 이름과 같습니다.");
+                                                        equalName = false;
+                                                        elseCheck1 = true; // 이거안하고 나가면 맨밑에 유효하지않은 URL이 출력되니까 써준다.
+                                                        break; //이름을 기존이름 그대로 바꾸면 그건 문제없으니 통과하도록 허용.
+                                                    }
+
+                                                    System.out.println("이미 해당 게시판이 존재합니다. 다른 이름을 입력해주세요");
                                                     System.out.println();
-                                                    equalName = true;
+                                                    existingName = true;
                                                     break;
                                                 }
+
+
                                             }
 
-                                        } while (equalName);
+                                        } while (existingName);
+
+                                        if(equalName){
 
                                         mapStorage.put(afterTitle, mapStorage.get(beforeTitle)); // 기존의 게시판 인스턴스 메모리주소를 그대로 복사해서 가져왔기때문에 게시판값이 그대로다.
                                         mapStorage.remove(beforeTitle); // 맵의 맵에서 기존 key(게시판제목) 를 가진 노드를 삭제. 실험결과 이렇게해도 afterTitle의 밸류는 그대로있음.
@@ -269,6 +300,7 @@ public class qwer2 {
 
                                         System.out.println("게시판 이름이 [" + afterTitle + "] 로 수정되었습니다!");
                                         elseCheck1 = true;
+                                        }
 
                                     }
 
